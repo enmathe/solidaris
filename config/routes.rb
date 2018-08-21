6c8a39c8e92cd7b64481c95bc0838bd8cfeed823
Lisiane Fonteneau <lisiane.fonteneau@live.fr>
2018-08-21 15:43:26 +0200

    début seeds organizations

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'organization/index'
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_for :users
@@ -8,12 +9,12 @@ Rails.application.routes.draw do
     resources :applications, only: [:create]
   end
 
-  resources :associations, only: [:index, :show]
-
   namespace :account do
     resource :profile, only: [:show, :edit, :update]
     resources :missions, only: [:index]
   end
+
+  resources :organizations, only: [:index]
 
   namespace :organizations do
     resources :missions do
