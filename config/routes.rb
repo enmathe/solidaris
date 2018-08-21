52ca78887d79268e0832c582450348f5d4d1f089
Lisiane Fonteneau <lisiane.fonteneau@live.fr>
2018-08-21 12:19:15 +0200

    ajout des routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,23 @@
 Rails.application.routes.draw do
-  devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users
+  devise_for :organizations
+
+  resources :missions, only: [:index, :show] do
+    resources :applications, only: [:create]
+  end
+
+  resources :associations, only: [:index, :show]
+
+  namespace :account do
+    resource :profile, only: [:show, :edit, :update]
+    resources :missions, only: [:index]
+  end
+
+  namespace :organizations do
+    resources :missions do
+      resources :applications, only: [:show]
+    end
+  end
 end
