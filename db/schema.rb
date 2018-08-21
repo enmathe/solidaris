1bf3427ae4004ca4cf99f1a41c70d30712c050ca
JulieYende <julieyende@gmail.com>
2018-08-21 11:47:29 +0200

    Add application_model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_20_204119) do
+ActiveRecord::Schema.define(version: 2018_08_21_094427) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "applications", force: :cascade do |t|
+    t.bigint "mission_id"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["mission_id"], name: "index_applications_on_mission_id"
+    t.index ["user_id"], name: "index_applications_on_user_id"
+  end
 
   create_table "missions", force: :cascade do |t|
     t.string "title"
@@ -81,5 +90,7 @@ ActiveRecord::Schema.define(version: 2018_08_20_204119) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "applications", "missions"
+  add_foreign_key "applications", "users"
   add_foreign_key "missions", "organizations"
 end
