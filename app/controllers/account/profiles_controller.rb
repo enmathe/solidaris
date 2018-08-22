7421cfcb0fd882fa1efa102dfb39bc04be9c7f54
Lisiane Fonteneau <lisiane.fonteneau@live.fr>
2018-08-22 14:31:27 +0200

    ébut controller account

---
+++ app/controllers/account/profiles_controller.rb
@@ -0,0 +1,33 @@
+class Account::ProfilesController < ApplicationController
+  before_action :set_user, only: [:show, :edit]
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    @profile = User.find(current_user.id)
+    @profile.update(user_params)
+    redirect_to account_profile_path(@profile), :notice => "Your changes have been saved!"
+  end
+
+  private
+
+  def set_user
+    @profile = User.find(current_user.id)
+  end
+
+  def user_params
+    params.require(:user).permit(
+      :first_name,
+      :last_name,
+      :phone_number,
+      :description,
+      :skills,
+      :experiences,
+      :picture
+    )
+  end
+end
