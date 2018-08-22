7d25e83eedfe32c3705717e3c970887e2bb51714
JulieYende <julieyende@gmail.com>
2018-08-22 16:22:11 +0200

    début de la méthode show

--- app/controllers/missions_controller.rb
+++ app/controllers/missions_controller.rb
@@ -2,4 +2,12 @@ class MissionsController < ApplicationController
   def index
     @missions = Mission.all
   end
+
+# en tant que user je peux voir les details d une mission
+  def show
+    @mission = Mission.find(params[:id])
+  end
 end
+
+
+
