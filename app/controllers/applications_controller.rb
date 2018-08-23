4b4eb046538bead891ad400596c65514a1b7281c
Lisiane Fonteneau <lisiane.fonteneau@live.fr>
2018-08-23 12:03:43 +0200

    la méthode post fonctionne

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -1,11 +1,13 @@
 class ApplicationsController < ApplicationController
+  before_action :authenticate_user!
+
   def create
     @application = Application.new
     @mission = Mission.find(params[:mission_id])
     @application.user_id = current_user.id
     @application.mission_id = @mission.id
     if @application.save
-      redirect_to account_profile_path(current_user)
+      redirect_to account_profile_path
     else
       render 'missions/show'
     end
