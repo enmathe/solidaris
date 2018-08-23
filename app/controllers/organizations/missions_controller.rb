32e02ba56e4fbc8ca6ae73d7dd0792d36a5ec479
Enzo Mathe <enzomathe@gmail.com>
2018-08-23 13:34:28 +0200

    design dashboard

--- app/controllers/organizations/missions_controller.rb
+++ app/controllers/organizations/missions_controller.rb
@@ -18,13 +18,13 @@ class Organizations::MissionsController < ApplicationController
 
 
     @missions.each do |mission|
-      if mission.starting_at < Date.current
+      if mission.starting_at > Date.current
         @coming << mission
-      elsif mission.recurrent == true && mission.starting_at > Date.current && mission.recurrency_ending_on < Date.current
+      elsif mission.recurrent == true && mission.starting_at < Date.current && mission.recurrency_ending_on > Date.current
         @recurrent_current << mission
-      elsif mission.recurrent == true && mission.recurrency_ending_on > Date.current
+      elsif mission.recurrent == true && mission.recurrency_ending_on < Date.current
         @past << mission
-      else mission.recurrent == false && mission.starting_at > Date.current
+      else mission.recurrent == false && mission.starting_at < Date.current
         @past << mission
       end
     end
