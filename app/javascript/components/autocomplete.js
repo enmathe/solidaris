41d8d4f5b4d80f531e0467cfd67ec2929d2a3d33
Lisiane Fonteneau <lisiane.fonteneau@live.fr>
2018-08-28 19:28:10 +0200

    debut autocomplete

--- app/javascript/components/autocomplete.js
+++ app/javascript/components/autocomplete.js
@@ -1,8 +1,10 @@
 function autocomplete() {
   document.addEventListener("DOMContentLoaded", function() {
-    var missionAddress = document.getElementsByClassName('mission_address');
-
+    console.log("liiiiiiiiiiiiiiiiiiiiiiiiiii");
+    var missionAddress = document.getElementById('mission_address');
+    // var flatAddress = document.querySelectorAll('address-auto');
     if (missionAddress) {
+      console.log("bouyaaahhh");
       var autocomplete = new google.maps.places.Autocomplete(missionAddress, { types: [ 'geocode' ] });
       google.maps.event.addDomListener(missionAddress, 'keydown', function(e) {
         if (e.key === "Enter") {
