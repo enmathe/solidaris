43c088d67ce096dab5a44cd80e759d35fa554ae7
Enzo Mathe <enzomathe@gmail.com>
2018-08-30 17:11:26 +0200

    finished

--- app/javascript/components/banner.js
+++ app/javascript/components/banner.js
@@ -2,7 +2,7 @@ import Typed from 'typed.js';
 
 function loadDynamicBannerText() {
   new Typed('#banner-typed-text', {
-    strings: ["La seule peine que vous encourez,", "c'est donnez de votre temps..."],
+    strings: ["La seule peine que vous encourez,", "c'est de donner de votre temps..."],
     typeSpeed: 50,
     loop: true
   });
