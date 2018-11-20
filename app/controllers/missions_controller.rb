9f26f589d8eba5533a2621ca36c161d49d98a8d3
Hugo Daniel <hugodaniel.icam@gmail.com>
2018-11-20 14:41:09 +0100

    Refacto search for missions#index with has_scope gem

--- app/controllers/missions_controller.rb
+++ app/controllers/missions_controller.rb
@@ -1,71 +1,15 @@
 class MissionsController < ApplicationController
+  has_scope :category
+  # has_scope :address
+  has_scope :recurrency
+  has_scope :time_range
 
   def index
-    @mission = Mission.new
-    @missions = Mission.all
+    @missions = apply_scopes(Mission).all.order(created_at: :desc)
 
-    address_user = params[:query]
-    if address_user.present?
-      @missions = @missions.near(address_user, 1)
-    end
-    # @missions.where("title ILIKE ?", "%#{params[:query]}%") if params[:query].present?
-    @missions = (params[:category].present? and params[:category] != "categories") ? @missions.where("category ILIKE ?", "#{params[:category]}") : @missions
-
-    missions_urg = []
-    @missions.each do |mission|
-      if mission.end_candidature_date != nil
-        if (mission.end_candidature_date < (Date.today - 7)) && mission.recurrent == false
-          missions_urg << mission
-        end
-      end
-    end
-
-    if params[:recurrency].present? and params[:recurrency] != "both"
-      if params['recurrency'] == "recurrent"
-        @missions = @missions.where(recurrent: true)
-      elsif params['recurrency'] == "ponctuel"
-        @missions = @missions.where(recurrent: false)
-      elsif params['recurrency'] == "urgent"
-        @missions = missions_urg
-      end
-    end
-
-
-
-
-
-
-    if params[:starting_at].present?
-      starting_on, ending_on = params[:starting_at].split(' au ')
-
-      starting_on = Date.strptime(starting_on, '%d.%m.%Y')
-      ending_on = Date.strptime(ending_on, '%d.%m.%Y')
-
-      @missions = @missions.where("date(starting_at) BETWEEN ? AND ?", starting_on, ending_on)
-    end
-    # mission[starting_at]=30.08.2018+to+31.08.2018
-
-    @missions = @missions.order(created_at: :desc)
-
-
-    unless @missions.empty?
-      @markers = marker(@missions.where.not(latitude: nil, longitude: nil))
-    end
+    @markers = marker(@missions.select { |mission| mission.is_localized }) unless @missions.empty?
   end
 
-    # @missions = Mission.all
-    # @coming = []
-    # @past=[]
-
-    # @missions.each do |mission|
-    #   if mission.starting_at > Date.current
-    #     @coming << mission
-    #   else mission.end_candidature_date < Date.current
-    #     @past << mission
-    #   end
-    # end
-
-# en tant que user je peux voir les details d une mission
   def show
     @mission = Mission.find(params[:id])
     @organization = @mission.organization
@@ -82,8 +26,4 @@ class MissionsController < ApplicationController
       }
     end
   end
-
 end
-
-
-
--- app/models/mission.rb
+++ app/models/mission.rb
@@ -6,17 +6,46 @@ class Mission < ApplicationRecord
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
 
+  scope :category, -> category { where("category ILIKE ?", category) unless category == "categories" }
+  # scope :address, -> city { near(address, 1) }
+  scope :recurrency, -> recurrency { scope_recurrency(recurrency) }
+  scope :time_range, -> time_range { scope_time_range(time_range) }
 
- extend Enumerize
- enumerize :category, in: [
-   :Hebergement,
-   :Activite,
-   :Collecte,
-   :Accompagnement,
-   :Evenement,
-   :Maraude,
-   :Enseignement,
-   :Sante
- ]
+
+  extend Enumerize
+  enumerize :category, in: [
+    :Hebergement,
+    :Activite,
+    :Collecte,
+    :Accompagnement,
+    :Evenement,
+    :Maraude,
+    :Enseignement,
+    :Sante
+  ]
+
+  def is_localized
+    self.longitude && self.latitude
+  end
+
+  def self.scope_recurrency(recurrency)
+    return unless recurrency.present? and recurrency != "both"
+    if recurrency == "recurrent"
+      self.where(recurrent: true)
+    elsif recurrency == "ponctuel"
+      self.where(recurrent: false)
+    elsif recurrency == "urgent"
+      self.where(recurrent: "false", end_candidature_date: (Date.today - 7)..Date.today)
+    end
+  end
+
+  def self.scope_time_range(time_range)
+    return unless time_range.present?
+    starting_on, ending_on = time_range.split(' au ')
+
+    starting_on = Date.strptime(starting_on, '%d.%m.%Y')
+    ending_on = Date.strptime(ending_on, '%d.%m.%Y')
+
+    self.where("date(starting_at) BETWEEN ? AND ?", starting_on, ending_on)
+  end
 end
-
